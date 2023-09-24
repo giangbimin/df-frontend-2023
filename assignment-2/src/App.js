5f5fcde3b93690252b0bccd858c9536093d3449e
giangbimin <gianghoang.dev@gmail.com>
2023-09-24 11:21:03 +0700

    A2 refactor

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import FormCreate from "./components/FormCreate";
@@ -8,68 +7,91 @@ import BookList from "./components/BookList";
 import BookManagerService from "./services/BookManagerService";
 import { DeleteBookFormContext } from "./contexts/DeleteBookFormContext";
 import Pagination from "./components/Pagination";
-;
 
 const App = () => {
+  // State variables
   const [theme, setTheme] = useState("light");
-  const [bookStore, setBookStore] = useState({ page: 1, totalPages: 0, books: []});
-  const [searchTerm, setSearchTerm] = useState({ term: "", page: 1, perPage: 5 });
+  const [bookStore, setBookStore] = useState({
+    page: 1,
+    totalPages: 0,
+    books: [],
+  });
+  const [searchTerm, setSearchTerm] = useState({
+    term: "",
+    page: 1,
+    perPage: 5,
+  });
   const [formCreate, setCreateForm] = useState({ isShow: false, book: null });
   const [formDelete, setDeleteForm] = useState({ isShow: false, book: null });
 
+  // Toggle theme handler
   const toggleTheme = () => {
-    setTheme((themeName) => (themeName === "light" ? "dark" : "light"));
-  }
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
 
+  // Set new search term handler
   const setNewSearchTerm = (newTerm, newPage, newPerPage) => {
     setSearchTerm((prevSearchTerm) => ({
       term: newTerm !== null ? newTerm : prevSearchTerm.term,
       page: newPage !== null ? newPage : prevSearchTerm.page,
       perPage: newPerPage !== null ? newPerPage : prevSearchTerm.perPage,
     }));
-  }
+  };
 
+  // Show create form handler
   const showCreateForm = () => {
-    setCreateForm({ isShow: true, book: null});
-  }
+    setCreateForm({ isShow: true, book: null });
+  };
 
+  // Close create form handler
   const closeCreateForm = () => {
     setCreateForm({ isShow: false, book: null });
-  }
+  };
 
+  // Show delete form handler
   const showDeleteForm = (book) => {
     setDeleteForm({ isShow: true, book: book });
-  }
+  };
 
+  // Close delete form handler
   const closeDeleteForm = () => {
     setDeleteForm({ isShow: false, book: null });
   };
 
+  // Add book handler
   const addBook = (book) => {
     BookManagerService.create(book);
     searchBook(searchTerm);
   };
 
+  // Delete book handler
   const deleteBook = (book) => {
-    BookManagerService.delete(book)
-    searchBook(searchTerm)
-  }
+    BookManagerService.delete(book);
+    searchBook(searchTerm);
+  };
 
+  // Handle search term change
   const onTermChange = (newTerm) => {
     setNewSearchTerm(newTerm, null, null);
   };
 
-    const onPageChange = (newPage) => {
-      console.log(newPage, bookStore.totalPages);
-      if (newPage > bookStore.totalPages || newPage < 1) return;
-      setNewSearchTerm(null, newPage, null);
-    };
-
-  const searchBook = async (curSearchTerm) => {
-    const result = BookManagerService.getList(curSearchTerm);
-    setBookStore({ books: result.data, page: result.page, totalPages: result.totalPages});
+  // Handle page change
+  const onPageChange = (newPage) => {
+    if (newPage > bookStore.totalPages || newPage < 1) return;
+    setNewSearchTerm(null, newPage, null);
   };
 
+  // Search for books based on search term
+  const searchBook = async (curSearchTerm) => {
+    const result = BookManagerService.getList(curSearchTerm);
+    setBookStore({
+      books: result.data,
+      page: result.page,
+      totalPages: result.totalPages,
+    });
+  };
+
+  // Effect to initiate search on component mount and when search term changes
   useEffect(() => {
     searchBook(searchTerm);
   }, [searchTerm]);
@@ -77,26 +99,26 @@ const App = () => {
   return (
     <div id="book-store" className={theme}>
       <header>
-          <Navbar theme={theme} toggleTheme={toggleTheme} />
+        <Navbar theme={theme} toggleTheme={toggleTheme} />
       </header>
       <main>
         <Header onTermChange={onTermChange} showCreateForm={showCreateForm} />
         <DeleteBookFormContext.Provider value={showDeleteForm}>
           <BookList books={bookStore.books} />
         </DeleteBookFormContext.Provider>
-        {bookStore.totalPages > 1 ? (
+        {bookStore.totalPages > 1 && (
           <Pagination bookStore={bookStore} onPageChange={onPageChange} />
-        ) : null}
-        {formDelete.isShow ? (
+        )}
+        {formDelete.isShow && (
           <FormDelete
             formDelete={formDelete}
             closeDeleteForm={closeDeleteForm}
             deleteBook={deleteBook}
           />
-        ) : null}
-        {formCreate.isShow ? (
+        )}
+        {formCreate.isShow && (
           <FormCreate closeCreateForm={closeCreateForm} addBook={addBook} />
-        ) : null}
+        )}
       </main>
     </div>
   );
