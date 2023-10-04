acdbc6c684ff0eebc312456fcac6483328377daa
giangbimin <gianghoang.dev@gmail.com>
2023-10-04 21:58:29 +0700

    A4 - fix bug

--- assignment-4/src/app/books/[id]/page.tsx
+++ assignment-4/src/app/books/[id]/page.tsx
@@ -82,7 +82,7 @@ export default function ShowBookPage({ params: { id } }) {
         <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
           <div className="w-full md:w-1/2">
             <h2 className="mb-4 text-xl tracking-tight font-extrabold  text-gray-900 dark:text-white">
-              Book {book.title} Detail:
+              Book {book.id} Detail:
             </h2>
           </div>
           <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
